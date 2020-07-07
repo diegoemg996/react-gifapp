370b3f739fe46be915faef190ff44545c0f509ff
Diego <diegoemg996@gmail.com>
2020-07-07 15:52:52 -0500

    docs agregado

---
+++ docs/precache-manifest.a717e6cb070fb8634e6188debd2970ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e144aadf725e0937b1c960a4665dcaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c49ade088eca577fa63",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c49ade088eca577fa63",
+    "url": "/static/js/main.f439980f.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
